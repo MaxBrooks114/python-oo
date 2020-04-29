4ed41524a637664bbf87f40a559d4bf5dc25c7ae
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-29 12:00:50 -0400

    constructors, self

--- oop.py
+++ oop.py
@@ -5,6 +5,9 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.make)
@@ -12,7 +15,14 @@ print(kenwood.price)
 
 kenwood.price = 12.75
 print(kenwood.price)
-
 hamilton = Kettle("Hamilton", 14.55)
-
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+print("Models: {0.make} = {0.price}".format(kenwood))
+hamilton.switch_on()
+Kettle.switch_on(kenwood)
+
+print("*" * 80)
+
+kenwood.power = 1.5
+
+print(kenwood.power)
