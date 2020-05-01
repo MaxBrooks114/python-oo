9ece4d528bbbf728010967aa1d7223c7747377d9
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-01 14:48:51 -0400

    oop in python

--- bankaccount.py
+++ bankaccount.py
@@ -9,43 +9,49 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
 
         print("Account created for " + self.name)
 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
+        if amount >= 0:
+            self.__balance += amount
             self.transaction_list.append((Account._current_time(), amount))
             self.show_balance()
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), amount))
+            self._balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))
         else:
             print("You broke, bitch.")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}.".format(self.balance))
+        print("Balance is {}.".format(self._balance))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
-            if amount > 0:
+        for date, amount in self._transaction_list:
+            if amount >= 0:
                 tran_type = "deposited"
             else:
                 tran_type = "withdrawn"
-            print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
-
+                amount *= -1
+            print("{:6} {} on {}, (local time was {})".format(amount, tran_type, date, date.astimezone()))
 
 
 if __name__ == '__main__':
+
     tim = Account("Tim", 0)
     tim.show_balance()
 
     tim.deposit(1000)
     tim.withdraw(500)
     tim.show_transactions()
+
+    steph = Account("Steph", 800)
+    steph.deposit(100)
+    steph.withdraw(200)
+    steph.show_transactions()
---
+++ song.py
@@ -0,0 +1,106 @@
+from __future__ import print_function
+
+class Song:
+
+    def __init__(self, title, artist, duration=0):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
+    def get_title(self):
+        return self.title
+
+    name = propert(get_title)
+
+class Album:
+
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various Artists")
+        else:
+            self.artist = artist
+
+        self.tracks = []
+
+    def add_song(self, song, position=None):
+        song_found = find_object(song, self.tracks)
+        if song_found is None:
+            song_found = Song(song, self.artist)
+            if position is None:
+                self.tracks.append(song_found)
+            else:
+                self.tracks.insert(position, song_found)
+
+
+class Artist:
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        if album not in self.albums:
+            self.albums.append(album)
+        else:
+            print("This album is already in the list")
+
+    def add_song(self, name, year, title):
+        """ Add a new song to the collection of albums"""
+        """ This method willl add the song to an album in the collection.
+        A new album will be created if it does not already exit
+        
+        Args:
+            name (str): The name of the album
+            year(int): The year the album was produced
+            title(str): the title of the song"""
+        album_found = find_object(name, self.albums)
+        if album_found is None:
+            print(name + " not found")
+            album_found = Album(name, year, self)
+            self.add_album(album_found)
+        else:
+            print("Found album " + name)
+
+        album_found.add_song(title)
+
+
+def find_object(field, object_list):
+    """ Check object list to see if object exists and return it if so  """
+    for item in object_list:
+        if item.name == field:
+            return item
+    return None
+
+
+def load_data():
+    artist_list = []
+
+    with open("albums.txt", "r") as albums:
+        for line in albums:
+            artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
+            year_field = int(year_field)
+            print("{}:{}:{}:{}".format(artist_field, album_field, year_field, song_field))
+
+            new_artist = find_object(artist_field, artist_list)
+            if new_artist is None:
+                new_artist = Artist(artist_field)
+                artist_list.append(new_artist)
+            new_artist.add_song(album_field, year_field, song_field)
+
+    return artist_list
+
+
+def create_checkfile(artist_list):
+    """create a check file from the object data for comparison with the original file"""
+    with open("checkfile.txt", 'w') as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.tracks:
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.name}".format(new_artist, new_album, new_song), file=checkfile)
+
+if __name__ == "__main__":
+    artists = load_data()
+    print("There are {} artists".format(len(artists)))
+    create_checkfile(artists)
