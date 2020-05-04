75c337f5b0783e756f4ae56b17e0708ff8f64c3f
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-04 16:58:02 -0400

    super class methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire
 
 ugly_troll = Troll("Pug")
 print("Ugly troll -{}".format(ugly_troll))
@@ -10,4 +10,17 @@ print("Another troll -{}".format(another_troll))
 brother = Troll("Ugr")
 print(brother)
 ugly_troll.grunt()
+
+brother.take_damage(4)
+print(brother)
+
+dracula = Vampire("Dracula")
+
+dracula.take_damage(3)
+
+print(dracula)
+
+
+while dracula.alive:
+    dracula.take_damage(3)
 
